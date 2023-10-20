43fc87c5c5b5e88edcc824548be75e709ce995e1
sristikundu1 <nidhukundu1234@gmail.com>
2023-10-20 16:31:47 +0600

    update the data in the database and show the updated data in th client side .

--- index.js
+++ index.js
@@ -47,7 +47,12 @@ async function run() {
       res.send(result);
     })
 
-   
+    app.get("/products/updateproduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result =await productCollection.findOne(query);
+      res.send(result);
+    })
 
     app.post("/products", async (req, res) => {
       const newProduct = req.body;
@@ -57,7 +62,27 @@ async function run() {
       res.send(result);
     })
 
-   
+    app.put("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateProduct = req.body;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const products = {
+        $set: {
+          name: updateProduct.name,
+          brandName: updateProduct.brandName,
+          type: updateProduct.type,
+          price: updateProduct.price,
+          rating: updateProduct.rating,
+          details: updateProduct.details,
+          photo: updateProduct.photo,
+         
+        }
+      }
+      const result = await productCollection.updateOne(filter, products, options);
+      res.send(result);
+
+    })
     
 
 
